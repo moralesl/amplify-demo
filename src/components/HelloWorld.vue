93d369f572507db8eb0a7bf244e8063ab1087178
Luis Morales <moralesl@users.noreply.github.com>
2022-07-26 16:52:57 +0200

    changed warning

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <b>Attention, this is a test</b>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -54,5 +55,9 @@ li {
 }
 a {
   color: #42b983;
+}
+b {
+  color: red;
+  font-size: 5em;
 }
 </style>
